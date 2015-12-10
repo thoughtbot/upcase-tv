27b40f77b83409529485d8e449ea07f13f5201ff
Giles Van Gruisen and Samuel Goodwin <gilesvangruisen@me.com>
2015-12-10 11:46:01 -0500

    init

---
+++ Upcase/AppDelegate.swift
@@ -0,0 +1,57 @@
+import UIKit
+import TVMLKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDelegate {
+
+  var window: UIWindow?
+  var appController: TVApplicationController?
+
+  static let TVBaseURL = "http://localhost:9001/"
+  static let TVBootURL = "\(AppDelegate.TVBaseURL)js/application.js"
+
+
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    window = UIWindow(frame: UIScreen.mainScreen().bounds)
+
+    let appControllerContext = TVApplicationControllerContext()
+
+    if let javaScriptURL = NSURL(string: AppDelegate.TVBootURL) {
+      appControllerContext.javaScriptApplicationURL = javaScriptURL
+    }
+
+    appControllerContext.launchOptions["BASEURL"] = AppDelegate.TVBaseURL
+
+    if let launchOptions = launchOptions as? [String: AnyObject] {
+      for (kind, value) in launchOptions {
+        appControllerContext.launchOptions[kind] = value
+      }
+    }
+
+    appController = TVApplicationController(context: appControllerContext, window: window, delegate: self)
+
+    return true
+  }
+
+  // MARK: TVApplicationControllerDelegate
+
+  func appController(appController: TVApplicationController, didFinishLaunchingWithOptions options: [String: AnyObject]?) {
+    print("\(__FUNCTION__) invoked with options: \(options)")
+  }
+
+  func appController(appController: TVApplicationController, didFailWithError error: NSError) {
+    print("\(__FUNCTION__) invoked with error: \(error)")
+
+    let title = "Error Launching Application"
+    let message = error.localizedDescription
+    let alertController = UIAlertController(title: title, message: message, preferredStyle:.Alert )
+
+    self.appController?.navigationController.presentViewController(alertController, animated: true, completion: { () -> Void in
+      // ...
+    })
+  }
+
+  func appController(appController: TVApplicationController, didStopWithOptions options: [String: AnyObject]?) {
+    print("\(__FUNCTION__) invoked with options: \(options)")
+  }
+}
